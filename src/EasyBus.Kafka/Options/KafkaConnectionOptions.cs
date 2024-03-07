91c771eeb938163b0360e330d09c27b018b29a7d
w12rr <dam.sibinski@gmail.com>
2024-03-07 08:27:39 +0100

    kafka first part

---
+++ src/EasyBus.Kafka/Options/KafkaConnectionOptions.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.Kafka;
+
+namespace EasyBus.Kafka.Options;
+
+public sealed class KafkaConnectionOptions
+{
+    public string[] BootstrapServers { get; set; } = Array.Empty<string>();
+    public SaslMechanism? SaslMechanism { get; set; } = Confluent.Kafka.SaslMechanism.Plain;
+    public SecurityProtocol? SecurityProtocol { get; set; } = Confluent.Kafka.SecurityProtocol.Plaintext;
+    public string SaslPassword { get; set; } = string.Empty;
+    public string SslKeyPassword { get; set; } = string.Empty;
+    public string SslKeystorePassword { get; set; } = string.Empty;
+    public string SaslUsername { get; set; } = string.Empty;
+}
---
+++ src/EasyBus.Kafka/Options/KafkaOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace EasyBus.Kafka.Options;
+
+public sealed class KafkaOptions
+{
+    public required IDictionary<string, KafkaConnectionOptions> Connections { get; init; }
+}
